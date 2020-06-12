cd9c3a9747ef436ec211af087f7233133836bb36
FishHD <lucas.straub2003@gmail.com>
2020-06-12 11:01:09 +0200

    JUnit Tests written

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -1,10 +1,11 @@
 package mars.rover;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
 
-class MarsRoverTest {
+import static org.junit.Assert.assertEquals;
+
+public class MarsRoverTest {
 
     @Test
     public void
@@ -18,6 +19,35 @@ class MarsRoverTest {
     acceptance_test_2() {
         String newPosition = MarsRover.move(3, 3, 'E', "MMRMMRMRRM");
         assertEquals("5 1 E", newPosition);
+    }
+
+    @Test
+    public void lucas_test_1() {
+        String newPosition = MarsRover.move(0, 0, 'S', "MMMMMMM");
+        assertEquals("0 0 S", newPosition);
+    }
+
+    @Test
+    public void lucas_test_2() {
+        String newPosition = MarsRover.move(0, 0, 'W', "MMMMMMMMMMMMMM");
+        assertEquals("0 0 W", newPosition);
+    }
+
+    @Test
+    public void lucas_test_3() {
+        String newPosition = MarsRover.move(1, 1, 'N', "MMMRM");
+        assertEquals("2 4 E", newPosition);
+    }
+
+    @Test
+    public void lucas_test_4() {
+        String newPosition = MarsRover.move(1, 1, 'N', "M");
+        assertEquals("1 2 N", newPosition);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void lucas_test_5() {
+        String newPosition = MarsRover.move(-1, -1, 'N', "M");
     }
 
 }
